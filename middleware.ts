7de240258487150371219526bf97c3eb0cd356ef
Samuel Okunribido <samuel.okunribido@icdfa.edu.ng>
2026-08-27 09:08:23 +0100

    Phase 1 tenant boundaries test

--- middleware.ts
+++ middleware.ts
@@ -5,8 +5,11 @@ import { authConfig } from "@/auth.config";
 const { auth } = NextAuth(authConfig);
 
 export const middleware = auth((request) => {
-	const response = NextResponse.next();
-	response.headers.set("x-request-id", crypto.randomUUID());
+	const requestId = crypto.randomUUID();
+	const requestHeaders = new Headers(request.headers);
+	requestHeaders.set("x-request-id", requestId);
+	const response = NextResponse.next({ request: { headers: requestHeaders } });
+	response.headers.set("x-request-id", requestId);
 	return response;
 });
 
